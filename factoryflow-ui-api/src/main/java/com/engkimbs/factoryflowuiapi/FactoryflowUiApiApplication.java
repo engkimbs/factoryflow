59be942e6afe6b331fce38a78c831c2f0c76ae33
engkimbs <eng.kimbs@gmail.com>
2022-08-11 21:28:25 +0900

    [Feature] API for factory flow UI

---
+++ factoryflow-ui-api/src/main/java/com/engkimbs/factoryflowuiapi/FactoryflowUiApiApplication.java
@@ -0,0 +1,13 @@
+package com.engkimbs.factoryflowuiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryflowUiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FactoryflowUiApiApplication.class, args);
+    }
+
+}
---
+++ factoryflow-ui-api/src/test/java/com/engkimbs/factoryflowuiapi/FactoryflowUiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engkimbs.factoryflowuiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryflowUiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
